19ac594100afaa79307c653a200c4e28f49f0cc6
ARSENIQ <merarseniebars99@gmail.com>
2025-07-12 19:56:53 +0300

    trafic

---
+++ Level_6_Special_task/Traffic_congestion/main.go
@@ -0,0 +1,34 @@
+package main
+
+type WeatherCondition int
+
+const (
+	Clear WeatherCondition = iota
+	Rain
+	HeavyRain
+	Snow
+)
+
+type WeatherData struct {
+	Condition WeatherCondition
+	WindSpeed int
+}
+
+func GetWeatherMultiplier(weather WeatherData) float64 {
+	cof := float64(Clear)
+
+	switch weather.Condition {
+	case HeavyRain:
+		cof *= 0.2
+	case Snow:
+		cof *= 0.15
+	case Rain:
+		cof *= 0.125
+	}
+
+	if weather.WindSpeed > 15 {
+		cof *= 0.1
+	}
+
+	return cof
+}
